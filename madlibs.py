eba5a980551ba1dba8ad21fc3740b8a4ebc995bc
Richmond Mpofu <93492119+riccchmond@users.noreply.github.com>
2024-01-25 09:46:44 +0200

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,29 @@
+# string concatenation project (Putting strings togteher)
+# different methods to concatenate strings 
+# madlib 1
+
+print("Mad Libs has started: A Camping We Will Go")
+noun1 = input("Noun:")  
+noun2 = input("Noun:")
+noun3 = input("Noun:")
+adj1 = input("Adjective:")
+verb1 = input("Verb:")
+adj2 = input("Adjective:")
+noun4 = input("Noun:")
+verb2 = input("Verb:")
+noun5 = input("Noun:")
+verb3 = input("Verb:")
+verb4 = input("Verb:")
+noun6 = input("Noun:")
+noun7 = input("Noun:")
+adj3 = input("Adjective:")
+noun8 = input("Noun:")
+
+madLib = f"\n This weekend I am going camping with {noun1}. I packed my lantern, \
+{noun2}, and {noun3}. I am so {adj1} to {verb1} in a tent. I am {adj2} we might see \
+a {noun4}, they are kind of dangerous. We are going to hike, fish, and {verb2}. \
+I have heard that the {noun5} lake is great for {verb3}. Then we will {verb4} \
+hike through the forrest for {noun6 }. If I see a {noun7} while hiking, I am going to bring \
+it home as a pet! At night we will tell {adj3} stories and roast {noun8} around the campfire. \n"
+
+print(madLib)
